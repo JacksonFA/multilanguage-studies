e49a558a049ff02d292d4a3eda6d87d88bc1be76
JacksonFA <jackson.f205@gmail.com>
2024-05-02 22:25:00 -0300

    new(frontend): update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,38 @@
+
 (async () => {
   for (let i = 3000; i <= 3011; i++) {
-    const response = await fetch(`http://localhost:${i}`);
-    const json = await response.json();
-    document.getElementById(i).innerHTML = json.message;
+    const response = await fetch(`http://localhost:${i}`)
+    const json = await response.json()
+    document.getElementById(i).innerHTML = json.message
   }
-})();
+})()
+
+// let counter = 0
+// const span = document.createElement('span')
+// span.id = 'counter'
+// span.innerHTML = '0'
+// const title = document.getElementById('title')
+// title.appendChild(span)
+// title.onclick = () => {
+//   increaseCounter()
+//   changeColor()
+// }
+// title.style.transition = 'color 500ms linear'
+// title.style.cursor = 'pointer'
+// title.addEventListener('mouseover', function() {
+//   title.style.color = '#4e7d8d'
+// })
+// title.addEventListener('mouseout', function() {
+//   title.style.color = '#fff'
+// })
+// function changeColor() {
+//   const title = document.getElementById('title')
+//   title.style.color = '#4e7d8d'
+//   title.classList.toggle('title-changed')
+//   title.classList.add('title-changed')
+//   title.classList.remove('title-changed')
+// }
+// function increaseCounter() {
+//   counter++
+//   document.getElementById('counter').innerText = counter
+// }
